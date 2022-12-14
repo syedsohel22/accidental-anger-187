a2e05de6cff3e518c0f186e71ede917fe10991db
sohel <mr.sohelsyed@gmail.com>
2022-12-14 22:15:25 +0530

    work done day-2

--- travelbook/src/Pages/Login.jsx
+++ travelbook/src/Pages/Login.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      Login
+      <h1>Login here.!</h1>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
